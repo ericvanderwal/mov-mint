29d5312f9b7505192c9e30d5e98598da1451c155
Eric Vander Wal <admin@uprisinglabs.io>
2025-05-21 21:02:26 -0700

    corrections for cloudflare pages

--- _worker.js
+++ _worker.js
@@ -1,9 +1,13 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    
+    // Handle static assets
     if (url.pathname.startsWith('/_next/static/')) {
       url.pathname = `/static${url.pathname.slice('/_next/static/'.length)}`;
     }
+    
+    // Serve the static site
     return env.ASSETS.fetch(url);
   }
 };
